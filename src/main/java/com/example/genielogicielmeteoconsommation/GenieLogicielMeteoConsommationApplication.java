908d0c22e2da537854ca5eb8d4939d182fd3371f
Alex HUANG <huang.alex2210@gmail.com>
2026-02-03 10:18:24 +0100

    Début du projet

---
+++ src/main/java/com/example/genielogicielmeteoconsommation/GenieLogicielMeteoConsommationApplication.java
@@ -0,0 +1,13 @@
+package com.example.genielogicielmeteoconsommation;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GenieLogicielMeteoConsommationApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GenieLogicielMeteoConsommationApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/genielogicielmeteoconsommation/GenieLogicielMeteoConsommationApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.genielogicielmeteoconsommation;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GenieLogicielMeteoConsommationApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
